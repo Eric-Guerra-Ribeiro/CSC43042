d849c1c15c1ea9660d15a5953eab901ae3c184ee
Eric Guerra Ribeiro <eric.guerra.07@gmail.com>
2025-03-26 08:50:28 +0100

    do analyses for quiz td32

--- TD3/quiz/hierarchical_clustering.py
+++ TD3/quiz/hierarchical_clustering.py
@@ -1,10 +1,60 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 from scipy.cluster.hierarchy import dendrogram, linkage
+from scipy.spatial.distance import squareform
 
-fname = '../csv/bluered.csv'
-data = pd.read_csv(fname, header = 0)
+def analyse_bluered():
+    fname = './csv/bluered.csv'
+    data = pd.read_csv(fname, header = 0)
 
-tree = linkage(data[['x', 'y']])
-D = dendrogram(tree, labels = data['name'].to_numpy(), orientation = 'left')
-plt.show()
+    tree = linkage(data[['x', 'y']])
+    D = dendrogram(tree, labels = data['name'].to_numpy(), orientation = 'left', color_threshold=2.0)
+    plt.show()
+
+
+def analyse_test6():
+    fname = './csv/test6.csv'
+    data = pd.read_csv(fname, header = 0)
+
+    tree = linkage(data[['x', 'y']])
+    D = dendrogram(tree, labels = data['name'].to_numpy(), orientation = 'left')
+    plt.show()
+
+
+def analyse_iris():
+    fname = './csv/iris.csv'
+    data = pd.read_csv(fname, header = 0)
+
+    tree = linkage(data[['a', 'b', 'c', 'd']])
+    plt.figure(figsize=(10, 10))
+    D = dendrogram(tree, labels = data['name'].to_numpy(), orientation = 'left')
+    plt.show()
+
+
+def analyse_languages():
+    def load_distance_matrix(fname):
+        """
+        Takes as input a name of a file containing the information about a graph:
+        - first line: number of vertices
+        - then labels, one per line
+        - then the distance matrix, one row per line, the entries separated by commas
+        Returns a tuple containing a distance matrix in the condensed 1-D format and a list of labels
+        """
+        with open(fname, 'r') as infile:
+            n = int(infile.readline())
+            labels = [infile.readline().strip() for _ in range(n)]
+            dist_matrix = [[float(x) for x in line.split(',')] for line in infile]
+            return (squareform(dist_matrix), labels)
+
+    fname = "./csv/languages.csv"
+    dist_matrix, labels = load_distance_matrix(fname)
+    tree = linkage(dist_matrix)
+    plt.figure(figsize=(10, 10))
+    D = dendrogram(tree, labels = labels, orientation = 'left')
+    plt.show()
+
+if __name__ == "__main__":
+    # analyse_bluered()
+    analyse_test6()
+    # analyse_iris()
+    # analyse_languages()
